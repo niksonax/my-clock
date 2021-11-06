6e1d41b7cc32eb2a4bad5656ca98eace580969ff
Nikita <nik2142ua@gmail.com>
2021-11-06 12:21:30 +0200

    Add ability to create/delete clock entities

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Clock from './components/Clock.jsx';
+import './App.css';
 
 const App = () => {
-  return <Clock />;
+  const [clocks, setClocks] = useState([]);
+
+  const addClock = () => setClocks((oldItems) => [...oldItems, <Clock />]);
+
+  const deleteClock = () => setClocks((old) => old.slice(0, -1));
+
+  const clocksRender = clocks.map((clock, index) => (
+    <div key={index} className="clock">
+      {clock}
+    </div>
+  ));
+
+  return (
+    <div className="container">
+      <div className="button-container">
+        <button id="add-btn" className="button" onClick={addClock}>
+          Add
+        </button>
+        <button id="delete-btn" className="button" onClick={deleteClock}>
+          Delete
+        </button>
+      </div>
+
+      <div className="clocks">{clocksRender}</div>
+    </div>
+  );
 };
 
 export default App;
